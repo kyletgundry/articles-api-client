9da7a596c58aab3c04396f6b2ca0748db81d212d
Kyle Gundry <Gundr1kt@gmail.com>
2017-04-04 21:30:25 -0500

    Add new and create routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,20 @@ class ArticlesController < ApplicationController
     render "index.html.erb"
   end
 
-  
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @article = Article.create(
+      title: params[:title],
+      author: params[:author],
+      image: params[:image],
+      date: params[:date],
+      description: params[:description]
+      )
+    redirect_to "/articles"
+  end
 
   def show
     @article = Unirest.get("http://localhost:3000/api/v1/articles/#{params[:id]}").body
